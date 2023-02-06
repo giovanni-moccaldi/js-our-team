876eb11c3be8954c1fa5e176cb0376f7c36ceb64
giovanni-moccaldi <giovannimoccaldi@gmail.com>
2023-02-06 15:36:37 +0100

    bidimensional

--- js/main.js
+++ js/main.js
@@ -1,54 +1,92 @@
 // Creare l’array di oggetti con le informazioni fornite.
-const team1 = {
-    nome: "Wayne Barnett",
-    role: "Founder & CEO",
-    picture: " wayne - barnett - founder - ceo.jpg",
-};
-// console.log(team1.nome);
-for (let info in team1) {
-    console.log(team1[info]);
-}
-const team2 = {
-    nome: "Angela Caroll",
-    role: "Chief Editor",
-    picture: " angela-caroll-chief-editor.jpg",
-};
-for (let info in team2) {
-    console.log(team2[info]);
-}
-const team3 = {
-    nome: "Walter Gordon",
-    role: "Office Manager",
-    picture: " walter-gordon-office-manager.jpg",
+// const team1 = {
+//     nome: "Wayne Barnett",
+//     role: "Founder & CEO",
+//     picture: " wayne - barnett - founder - ceo.jpg",
+// };
+// // console.log(team1.nome);
+// for (let info in team1) {
+//     console.log(team1[info]);
+// }
+// const team2 = {
+//     nome: "Angela Caroll",
+//     role: "Chief Editor",
+//     picture: " angela-caroll-chief-editor.jpg",
+// };
+// for (let info in team2) {
+//     console.log(team2[info]);
+// }
+// const team3 = {
+//     nome: "Walter Gordon",
+//     role: "Office Manager",
+//     picture: " walter-gordon-office-manager.jpg",
 
-}
-for (let info in team3) {
-    console.log(team3[info]);
-}
-const team4 = {
-    nome: "Angela Lopez	",
-    role: "Social Media Manager",
-    picture: "angela-lopez-social-media-manager.jpg",
+// }
+// for (let info in team3) {
+//     console.log(team3[info]);
+// }
+// const team4 = {
+//     nome: "Angela Lopez	",
+//     role: "Social Media Manager",
+//     picture: "angela-lopez-social-media-manager.jpg",
 
-}
-for (let info in team4) {
-    console.log(team4[info]);
-}
-const team5 = {
-    nome: "Scott Estrada",
-    role: "	Developer",
-    picture: "scott-estrada-developer.jpg",
+// }
+// for (let info in team4) {
+//     console.log(team4[info]);
+// }
+// const team5 = {
+//     nome: "Scott Estrada",
+//     role: "	Developer",
+//     picture: "scott-estrada-developer.jpg",
 
-}
-for (let info in team5) {
-    console.log(team5[info]);
-}
-const team6 = {
-    nome: "Barbara Ramos",
-    role: "	Graphic Designer",
-    picture: "barbara-ramos-graphic-designer.jpg",
+// }
+// for (let info in team5) {
+//     console.log(team5[info]);
+// }
+// const team6 = {
+//     nome: "Barbara Ramos",
+//     role: "	Graphic Designer",
+//     picture: "barbara-ramos-graphic-designer.jpg",
 
-}
-for (let info in team6) {
-    console.log(team6[info]);
-}
+// }
+// for (let info in team6) {
+//     console.log(team6[info]);
+// }
+const teamBrand = [
+    {
+        nome: "Wayne Barnett",
+        role: "Founder & CEO",
+        picture: " wayne - barnett - founder - ceo.jpg"
+    }
+    {
+        nome: "Angela Caroll",
+        role: "Chief Editor",
+        picture: " angela-caroll-chief-editor.jpg",
+    }
+    {
+
+        nome: "Walter Gordon",
+        role: "Office Manager",
+        picture: " walter-gordon-office-manager.jpg",
+    }
+
+    {
+        nome: "Angela Lopez	",
+        role: "Social Media Manager",
+        picture: "angela-lopez-social-media-manager.jpg",
+
+    }
+{
+        nome: "Scott Estrada",
+        role: "	Developer",
+        picture: "scott-estrada-developer.jpg",
+
+    }
+{
+        nome: "Barbara Ramos",
+        role: "	Graphic Designer",
+        picture: "barbara-ramos-graphic-designer.jpg",
+
+    }
+
+]
